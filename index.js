d1edf5d8a499dfb439076bcaa9e811f27415e880
Yash Dnyaneshwar Shelke <yashshelke885@gmail.com>
2024-06-28 09:17:36 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,338 @@
+const courses = [
+	{
+		id: 1,
+		image: "images/pexels-aliona-pasha-1687920-3238764.jpg",
+		title: "Integration of 3D Elements",
+		content:
+			"The integration of three-dimensional (3D) elements in web design is gaining popularity",
+		by: "Emily Johnson",
+		price: 99,
+		lessons: 22,
+		level: "Beginner",
+		main: `Web designers are leveraging technologies like WebGL to seamlessly incorporate 3D graphics, animations, and interactive elements into websites. This trend not only adds a new dimension to visual storytelling but also enhances user engagement, especially in industries such as gaming, e-commerce, and creative portfolios.The integration of 3D elements in design has emerged as a transformative trend, bringing a new dimension to visual experiences across various digital platforms. Designers are increasingly leveraging technologies like WebGL to seamlessly incorporate three-dimensional graphics, animations, and interactive elements into websites and applications. This trend transcends traditional flat design, offering a more immersive and engaging user experience. From product showcases to interactive storytelling, the integration of 3D elements enhances visual appeal and captivates audiences. This shift towards three-dimensional design reflects a dynamic approach, enabling designers to create visually stunning and interactive digital spaces that leave a lasting impression on users.`,
+		desc: `The integration of three-dimensional (3D) elements in web design is gaining popularity, offering a visually engaging and immersive experience. `,
+		lang: "English",
+	},
+	{
+		id: 2,
+		image: "images/pexels-chente8888-26614294.jpg",
+		title: "Dark Mode Design Trends",
+		content:
+			"The integration of three-dimensional (3D) elements in web design is gaining popularity",
+		by: "James Parker",
+		price: 70,
+		lessons: 10,
+		level: "Moderate",
+		main: `Web designers are leveraging technologies like WebGL to seamlessly incorporate 3D graphics, animations, and interactive elements into websites. This trend not only adds a new dimension to visual storytelling but also enhances user engagement, especially in industries such as gaming, e-commerce, and creative portfolios.The integration of 3D elements in design has emerged as a transformative trend, bringing a new dimension to visual experiences across various digital platforms. Designers are increasingly leveraging technologies like WebGL to seamlessly incorporate three-dimensional graphics, animations, and interactive elements into websites and applications. This trend transcends traditional flat design, offering a more immersive and engaging user experience. From product showcases to interactive storytelling, the integration of 3D elements enhances visual appeal and captivates audiences. This shift towards three-dimensional design reflects a dynamic approach, enabling designers to create visually stunning and interactive digital spaces that leave a lasting impression on users.`,
+		desc: `The integration of three-dimensional (3D) elements in web design is gaining popularity, offering a visually engaging and immersive experience. `,
+		lang: "English",
+	},
+	{
+		id: 3,
+		image: "images/pexels-helloaesthe-17295671.jpg",
+		title: "Dark Mode Design Trends",
+		content:
+			"The integration of three-dimensional (3D) elements in web design is gaining popularity",
+		by: "James Parker",
+		price: 70,
+		lessons: 10,
+		level: "Moderate",
+		main: `Web designers are leveraging technologies like WebGL to seamlessly incorporate 3D graphics, animations, and interactive elements into websites. This trend not only adds a new dimension to visual storytelling but also enhances user engagement, especially in industries such as gaming, e-commerce, and creative portfolios.The integration of 3D elements in design has emerged as a transformative trend, bringing a new dimension to visual experiences across various digital platforms. Designers are increasingly leveraging technologies like WebGL to seamlessly incorporate three-dimensional graphics, animations, and interactive elements into websites and applications. This trend transcends traditional flat design, offering a more immersive and engaging user experience. From product showcases to interactive storytelling, the integration of 3D elements enhances visual appeal and captivates audiences. This shift towards three-dimensional design reflects a dynamic approach, enabling designers to create visually stunning and interactive digital spaces that leave a lasting impression on users.`,
+		desc: `The integration of three-dimensional (3D) elements in web design is gaining popularity, offering a visually engaging and immersive experience. `,
+		lang: "English",
+	},
+	{
+		id: 4,
+		image: "images/pexels-tahmetler-24709246.jpg",
+		title: "Dark Mode Design Trends",
+		content:
+			"The integration of three-dimensional (3D) elements in web design is gaining popularity",
+		by: "James Parker",
+		price: 70,
+		lessons: 10,
+		level: "Moderate",
+		main: `Web designers are leveraging technologies like WebGL to seamlessly incorporate 3D graphics, animations, and interactive elements into websites. This trend not only adds a new dimension to visual storytelling but also enhances user engagement, especially in industries such as gaming, e-commerce, and creative portfolios.The integration of 3D elements in design has emerged as a transformative trend, bringing a new dimension to visual experiences across various digital platforms. Designers are increasingly leveraging technologies like WebGL to seamlessly incorporate three-dimensional graphics, animations, and interactive elements into websites and applications. This trend transcends traditional flat design, offering a more immersive and engaging user experience. From product showcases to interactive storytelling, the integration of 3D elements enhances visual appeal and captivates audiences. This shift towards three-dimensional design reflects a dynamic approach, enabling designers to create visually stunning and interactive digital spaces that leave a lasting impression on users.`,
+		desc: `The integration of three-dimensional (3D) elements in web design is gaining popularity, offering a visually engaging and immersive experience. `,
+		lang: "English",
+	},
+	{
+		id: 5,
+		image: "images/pexels-zetong-li-880728-13332942.jpg",
+		title: "Dark Mode Design Trends",
+		content:
+			"The integration of three-dimensional (3D) elements in web design is gaining popularity",
+		by: "James Parker",
+		price: 70,
+		lessons: 10,
+		level: "Moderate",
+		main: `Web designers are leveraging technologies like WebGL to seamlessly incorporate 3D graphics, animations, and interactive elements into websites. This trend not only adds a new dimension to visual storytelling but also enhances user engagement, especially in industries such as gaming, e-commerce, and creative portfolios.The integration of 3D elements in design has emerged as a transformative trend, bringing a new dimension to visual experiences across various digital platforms. Designers are increasingly leveraging technologies like WebGL to seamlessly incorporate three-dimensional graphics, animations, and interactive elements into websites and applications. This trend transcends traditional flat design, offering a more immersive and engaging user experience. From product showcases to interactive storytelling, the integration of 3D elements enhances visual appeal and captivates audiences. This shift towards three-dimensional design reflects a dynamic approach, enabling designers to create visually stunning and interactive digital spaces that leave a lasting impression on users.`,
+		desc: `The integration of three-dimensional (3D) elements in web design is gaining popularity, offering a visually engaging and immersive experience. `,
+		lang: "English",
+	},
+	{
+		id: 6,
+		image: "images/pexels-dawid-kochman-129685123-24709890.jpg",
+		title: "Dark Mode Design Trends",
+		content:
+			"The integration of three-dimensional (3D) elements in web design is gaining popularity",
+		by: "James Parker",
+		price: 70,
+		lessons: 10,
+		level: "Moderate",
+		main: `Web designers are leveraging technologies like WebGL to seamlessly incorporate 3D graphics, animations, and interactive elements into websites. This trend not only adds a new dimension to visual storytelling but also enhances user engagement, especially in industries such as gaming, e-commerce, and creative portfolios.The integration of 3D elements in design has emerged as a transformative trend, bringing a new dimension to visual experiences across various digital platforms. Designers are increasingly leveraging technologies like WebGL to seamlessly incorporate three-dimensional graphics, animations, and interactive elements into websites and applications. This trend transcends traditional flat design, offering a more immersive and engaging user experience. From product showcases to interactive storytelling, the integration of 3D elements enhances visual appeal and captivates audiences. This shift towards three-dimensional design reflects a dynamic approach, enabling designers to create visually stunning and interactive digital spaces that leave a lasting impression on users.`,
+		desc: `The integration of three-dimensional (3D) elements in web design is gaining popularity, offering a visually engaging and immersive experience. `,
+		lang: "English",
+	},
+];
+
+const cardDiv = document.getElementById("courses");
+let html = ``;
+courses.map((course, index) => {
+	if (index == 4) return;
+	html += `<div class="card" data-id="${course.id}">
+						<img
+							src="${course.image}"
+							alt=""
+							width="300px"
+						/>
+						<div class="cart-title">
+							<h4>${course.title}</h4>
+						</div>
+						<div class="content">
+							<p>${course.content}</p>
+						</div>
+						<hr />
+						<div class="card-f1">
+							<p>By <span>${course.by}</span></p>
+							<p>${course.price}$</p>
+						</div>
+						<hr />
+						<div class="card-f2">
+							<p>Lessons : <span>${course.lessons}</span></p>
+							<p>Level : <span>${course.level}</span></p>
+							<button class="btn" data-id="${course.id}">-></button>
+						</div>
+					</div>`;
+});
+
+cardDiv.innerHTML = html;
+const allCourseBtn = document.querySelector(".all-course-btn");
+let currCourse;
+const btn = document.querySelectorAll(".btn");
+let courseDetails;
+btn.forEach((b) => {
+	b.addEventListener("click", (e) => {
+		const id = e.target.dataset.id;
+		courseDetails = courses.filter((course) => {
+			return id == course.id;
+		});
+		currCourse = id;
+		setDetails(courseDetails);
+		allCourseBtn.style.display = "none";
+	});
+});
+let index;
+function goNext() {
+	let index = courses.findIndex((course) => {
+		return course.id == currCourse;
+	});
+	index = (index + 1) % courses.length;
+	setNextDetails(index);
+}
+
+const detailsDiv = document.querySelector("#details");
+function setDetails(courseDetails) {
+	currCourse = courseDetails[0].id;
+	detailsDiv.innerHTML = `<div id="details">
+					<div><button class="back-btn" onClick="goBack()"> <-</button></div>
+					<div class="course-img">
+						<img
+							src="${courseDetails[0].image}"
+							alt=""
+							width="100%"
+						/>
+					</div>
+					<div class="course-content">
+						<div class="course-details">
+							<h2>${courseDetails[0].title}</h2>
+							<p>${courseDetails[0].desc}</p>
+							<p>${courseDetails[0].main}</p>
+						</div>
+						<div class="course-card">
+							<h4>Course Details</h4>
+							<div>
+								<p>Author :</p>
+								<p>${courseDetails[0].by}</p>
+							</div>
+							<hr />
+							<div>
+								<p>Level :</p>
+								<p>${courseDetails[0].level}</p>
+							</div>
+							<hr />
+							<div>
+								<p>lessons :</p>
+								<p>${courseDetails[0].lessons}</p>
+							</div>
+							<hr />
+							<div>
+								<p>Language :</p>
+								<p>${courseDetails[0].lang}</p>
+							</div>
+                            <hr />
+							<div>
+								<p>Price :</p>
+								<p>${courseDetails[0].price}$</p>
+							</div>
+							<div class="course-btn-div">
+								<button>Buy Now</button>
+							</div>
+						</div>
+					</div>
+                    <div><button class="next-btn" onClick="goNext()"> next course</button></div>
+				</div>`;
+	detailsDiv.style.display = "block";
+	document.querySelectorAll(".main-info").forEach((div) => {
+		div.style.display = "none";
+	});
+	cardDiv.style.display = "none";
+}
+function setNextDetails(course) {
+	course = courses[course];
+	currCourse = course.id;
+	detailsDiv.innerHTML = `<div id="details">
+					<div><button class="back-btn" onClick="goBack()"> <-</button></div>
+					<div class="course-img">
+						<img
+							src="${course.image}"
+							alt=""
+							width="100%"
+						/>
+					</div>
+					<div class="course-content">
+						<div class="course-details">
+							<h2>${course.title}</h2>
+							<p>${course.desc}</p>
+							<p>${course.main}</p>
+						</div>
+						<div class="course-card">
+							<h4>Course Details</h4>
+							<div>
+								<p>Author :</p>
+								<p>${course.by}</p>
+							</div>
+							<hr />
+							<div>
+								<p>Level :</p>
+								<p>${course.level}</p>
+							</div>
+							<hr />
+							<div>
+								<p>lessons :</p>
+								<p>${course.lessons}</p>
+							</div>
+							<hr />
+							<div>
+								<p>Language :</p>
+								<p>${course.lang}</p>
+							</div>
+                            <hr />
+							<div>
+								<p>Price :</p>
+								<p>${course.price}$</p>
+							</div>
+							<div class="course-btn-div">
+								<button>Buy Now</button>
+							</div>
+						</div>
+					</div>
+                    </div>
+                    <div><button class="next-btn" onClick="goNext()"> next course</button></div>
+                    `;
+	detailsDiv.style.display = "block";
+	document.querySelectorAll(".main-info").forEach((div) => {
+		div.style.display = "none";
+	});
+	cardDiv.style.display = "none";
+}
+
+function goBack() {
+	detailsDiv.style.display = "none";
+	document.querySelectorAll(".main-info").forEach((div) => {
+		div.style.display = "block";
+	});
+	cardDiv.style.display = "flex";
+	allCourseBtn.style.display = "block";
+}
+
+const closeBtn = document.querySelector(".close-btn");
+const menuBtn = document.querySelector(".menu-btn");
+const sidebar = document.querySelector(".sidebar-res ul");
+
+menuBtn.addEventListener("click", () => {
+	sidebar.style.display = "block";
+	closeBtn.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+	sidebar.style.display = "none";
+	closeBtn.style.display = "none";
+});
+
+const faq = [
+	{
+		id: 1,
+		question: "whats are iCourses?",
+		answer: "educational courses offerred through various platforms",
+	},
+	{
+		id: 2,
+		question: "whats are iCourses?",
+		answer: "educational courses offerred through various platforms",
+	},
+	{
+		id: 3,
+		question: "whats are iCourses?",
+		answer: "educational courses offerred through various platforms",
+	},
+	{
+		id: 4,
+		question: "whats are iCourses?",
+		answer: "educational courses offerred through various platforms",
+	},
+	{
+		id: 5,
+		question: "whats are iCourses?",
+		answer: "educational courses offerred through various platforms",
+	},
+];
+
+const accordion = document.querySelector(".accordion");
+let accHtml = `<ul>`;
+
+faq.map((faq) => {
+	accHtml += `
+			<li>
+				<div class="accordion">
+                    <div class="accordion-que">${faq.question}</div>
+                    <div class="accordion-btn">🔽</div>
+                </div>
+				<div class="accordion-ans" data-id="${faq.id}">${faq.answer}</div>
+			</li>
+		`;
+});
+
+accordion.innerHTML = accHtml + "</ul>";
+const accDiv = document.querySelectorAll(".accordion-ans");
+document
+	.querySelectorAll(".accordion-btn")
+	.forEach((btn) => btn.addEventListener("click", openAccordion));
+
+function openAccordion(e) {
+	const div = e.target.parentElement.nextElementSibling;
+	if (div.style.display == "block") {
+		div.style.display = "none";
+	} else {
+		div.style.display = "block";
+	}
+}
